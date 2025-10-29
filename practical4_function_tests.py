f8d87b2628f1d900fda65bdd5d418f3680c39b08
averyordner <a_ordner@coloradocollege.edu>
2025-10-29 11:39:43 -0600

    practical 4 - ran my function for 8 datasets

---
+++ practical4_function_tests.py
@@ -0,0 +1,6 @@
+import KRDU_temp_import as kti
+path = 'C:\\Users\\avery\\Downloads\\ev228_data\\practical4_data\\'
+fn = 'USW00093009_temp_190801-202508.csv'
+column = 'JAN'
+kti.fun_KRDU_column(path, fn, column)
+
