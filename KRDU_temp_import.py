3fb8ccf584e49b13134b6f1d47536222ddc67ebd
averyordner <a_ordner@coloradocollege.edu>
2025-10-30 10:20:02 -0600

    update of KRDU_temp_import

--- KRDU_temp_import.py
+++ KRDU_temp_import.py
@@ -4,13 +4,13 @@ import pandas as pd
 path = 'C:\\Users\\avery\\Downloads\\ev228_data\\'
 fn = 'KRDU_temp_188708-202508.csv'
 
-KRDU_temp_data = pd.read_csv(path + fn)
+df = pd.read_csv(path + fn)
 
-fall_temp = KRDU_temp_data['S-O-N']
+fall_temp = df['S-O-N']
 
-def fun_KRDU_column(path, fn, column):
-   KRDU_temp_data = pd.read_csv(path + fn)
-   variable_column = KRDU_temp_data[column]
+def fun_extract_column(path, fn, column):
+   df = pd.read_csv(path + fn)
+   variable_column = df[column]
    print(variable_column)
 
-fun_KRDU_column(path, fn, 'S-O-N')
+fun_extract_column(path, fn, 'S-O-N')
