1ce9ac8a162540732d35bb85beddc801757b5f6d
averyordner <a_ordner@coloradocollege.edu>
2025-10-30 11:05:59 -0600

    using xarray to extract a variable and plot

---
+++ ERA5_2mtemp_prac.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import xarray as xr
+
+path = 'C:\\Users\\avery\\Downloads\\ev228_data\\'
+fn = 'ERA5_t2m_1997_2025.nc'
+
+ds= xr.open_dataset(path + fn, engine="netcdf4")
+print(ds)
+
+da = ds['t2m']
+
+da.plot();plt.show()
+
