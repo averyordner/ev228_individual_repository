8466b72cfba5440091f29e0dfceaf016cd8b86e6
averyordner <a_ordner@coloradocollege.edu>
2025-11-03 12:38:53 -0700

    ERA5 2m temp prac

--- ERA5_2mtemp_prac.py
+++ ERA5_2mtemp_prac.py
@@ -11,7 +11,37 @@ print(ds)
 
 da = ds['t2m']
 
-da_t2m_timestat = da.max('valid_time')
+#da_t2m_timestat = da.max('valid_time')
 
-da_t2m_timestat.plot();plt.show()
+#da_t2m_timestat.plot();plt.show()
 
+def fun_grid_variable(path, fn, column):
+    ds= xr.open_dataset(path + fn, engine="netcdf4")
+    variable_column = ds[column]
+    print(variable_column)
+    return variable_column 
+
+def descrip_statistics(variable_column, da):
+    dict_stat = {
+        1: da.mean(variable_column),
+        2: da.max(variable_column),
+        3: da.min(variable_column),
+        4: da.std(variable_column)
+    }
+    return dict_stat
+
+def plot_grid_data(dict_stat):
+    dict_stat.plot()
+    plt.title('t2m')
+    plt.show()
+
+
+    
+    
+
+
+
+
+
+
+    
