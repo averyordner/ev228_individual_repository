ac7c6be514dd956c49c5539b79fe685174d543b6
averyordner <a_ordner@coloradocollege.edu>
2025-11-06 22:18:24 -0700

    plot updates

--- fun_plots.py
+++ fun_plots.py
@@ -12,9 +12,9 @@ def timeseries(in_df, in_x=None, out_path='', out_name=''):
 
     plt.plot(in_x, in_df, color='#dc6b2b', linewidth=2.5)
     plt.xlabel('years')
-    plt.xlim(1892, 2025)
+    plt.xlim(1948, 2025)
     plt.ylabel('annual temperature (deg C)')
-    plt.title('SGM00061600 Saint-Louis, Senegal 1892-2025')
+    plt.title('ASM00094998 Macquarie Island, Australia 1948-2025')
     plt.savefig(out_path + out_name, dpi=400)
 
 def map(in_da, out_path='', out_name=''):
@@ -27,7 +27,7 @@ def map(in_da, out_path='', out_name=''):
     image = plt.pcolormesh(lons, lats, in_da)
     plt.xlabel('longitude')
     plt.ylabel('latitude')
-    plt.title('ERA5 10m Wind 1980-1989 Mean')
+    plt.title('Mean Surface Runoff Rate for Houston, Texas')
     cb = plt.colorbar(image, shrink=.75, orientation="vertical", pad=.02)
-    cb.set_label('wind speed in m/s')
+    cb.set_label('surface runoff rate')
     plt.savefig(out_path + out_name, dpi=400)
