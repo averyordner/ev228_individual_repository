e5c92199f8424bb405694f79fa6a214e438c3051
averyordner <a_ordner@coloradocollege.edu>
2025-11-07 15:19:11 -0700

    updates for individual research project

--- indiv_research_proj.py
+++ indiv_research_proj.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+import cartopy.crs as ccrs
 
 import fun_imports as fi
 import fun_plots as fp
@@ -10,7 +11,20 @@ fn = 'data_stream-moda_stepType-avgad.nc'
 out_path = 'C:\\Users\\avery\\Downloads\\ev228_data\\graphs\\'
 out_fn = '1-surface_runoff.png'
 
-da = fi.import_era5(file_path=path + fn, var='avg_surfror')
-da_timemn = da.mean(dim='valid_time')
+ds= xr.open_dataset(path + fn, engine="netcdf4")
 
-fp.map(da_timemn, out_path=out_path, out_name=out_fn)
+da = fi.import_era5(file_path=path + fn, var='avg_surfror')
+da_time = ds['valid_time']
+#print(da_time)
+
+specific_timeslices = da.isel(valid_time=[0, 3, 7])
+print("\nSpecific timeslices at indices 0, 3, 7:")
+print(specific_timeslices)
+
+#fp.map(da_timemn, out_path=out_path, out_name=out_fn)
+
+
+
+
+
+
