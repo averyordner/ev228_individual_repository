77c0074a7403d4056fa12e98371ca9edee52ab1f
averyordner <a_ordner@coloradocollege.edu>
2025-11-08 13:23:44 -0700

    updates to individual research project code

--- indiv_research_proj.py
+++ indiv_research_proj.py
@@ -13,16 +13,29 @@ out_fn = '1-surface_runoff.png'
 
 ds= xr.open_dataset(path + fn, engine="netcdf4")
 
-da = fi.import_era5(file_path=path + fn, var='avg_surfror')
+da_runoff = fi.import_era5(file_path=path + fn, var='avg_surfror')
 da_time = ds['valid_time']
 #print(da_time)
 
-specific_timeslices = da.isel(valid_time=[0, 360, 720, 1020])
-print("\nSpecific timeslices at indices 0, 360, 720, 1020:")
-print(specific_timeslices)
+specific_timeslices = da_runoff.isel(valid_time=[0, 360, 720, 1020])
+#print("\nSpecific timeslices at indices 0, 360, 720, 1020:")
+#print(specific_timeslices)
 
-#fp.map(da_timemn, out_path=out_path, out_name=out_fn)
+def fun_cartopy_basemap(specific_timeslices):
+    specific_timeslices.plot()
+    fig = plt.figure(figsize=(10,10))
+    imagery = OSM()
+    ax = plt.axes(projection=imagery.crs, )
+    ax.set_extent((31, -97, 28, -94))
+    # Add the imagery to the map.
+    zoom = 12
+    ax.add_image(imagery, zoom)
+    plt.title('Map of Houston, Texas and Surrounding Areas')
+    plt.show()
 
+
+
+#fp.map(specific_timeslices, out_path=out_path, out_name=out_fn)
 
 
 
